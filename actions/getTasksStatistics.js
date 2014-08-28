0420e06ea38b58f5d9931c10b15327eda4c85ea5
Sebastian Dechant <sebshome@web.de>
2014-08-28 21:53:10 +0200

    updated Tasks Status Widget

---
+++ actions/getTasksStatistics.js
@@ -0,0 +1,44 @@
+var async = require('async');
+var action = {};
+
+/////////////////////////////////////////////////////////////////////
+// metadata
+action.name = 'getTasksStatistics';
+action.description = 'I will return all statistics for runned tasks';
+action.inputs = {
+  'required': [],
+  'optional': []
+};
+action.blockedConnectionTypes = [];
+action.outputExample = {
+}
+
+/////////////////////////////////////////////////////////////////////
+// functional
+action.run = function (api, connection, next) {
+    // Get all workers
+  api.resque.scheduler.connection.redis.smembers('resque:workers', function(err, workers) {
+    var processedJobs = {};
+    api.resque.scheduler.connection.redis.get('resque:stat:processed', function(err, processedOverallJob) {
+      processedJobs["All"] = processedOverallJob;
+      connection.response.processedJobs = processedJobs;
+    });
+    // Iterate through each worker strint
+    async.each(workers, function( worker, callback) {
+      // get the current stats for the Worker
+      var splittedWorker = worker.split(':');
+      api.resque.scheduler.connection.redis.get('resque:stat:processed:' + splittedWorker[0] + ':' + splittedWorker[1], function(err, processedJob) {
+        processedJobs[splittedWorker[0] + ':' + splittedWorker[1]] = processedJob;
+        callback();
+      });
+    }, function(err){
+      // At the end return all running jobs for all workers
+      connection.response.processedJobs = processedJobs;
+      next(connection, true);
+    });
+  });
+
+};
+/////////////////////////////////////////////////////////////////////
+// exports
+exports.action = action;
--- public/dashboard/js/directives/widgets/ahTasksStats.js
+++ public/dashboard/js/directives/widgets/ahTasksStats.js
@@ -4,7 +4,16 @@ define(['app'], function(app){
       restrict: 'A',
       scope: true,
       replace: true,
-      template: '<div><ul class="list-group"><a href="#/tasks" class="list-group-item list-group-item-warning">Running Jobs<span class="badge">{{runningJobs}}</span></a><a href="#/tasks" class="list-group-item list-group-item-info">Delayed Jobs<span class="badge">{{delayedJobs}}</span></a><a href="#/tasks" class="list-group-item list-group-item-danger">Failed Jobs<span class="badge">{{failedJobs}}</span></a></ul></div>',
+      template: '<div>'+
+                  '<ul class="list-group">'+
+                    '<a href="#/tasks" class="list-group-item list-group-item-warning">Running Jobs<span class="badge">{{runningJobs}}</span></a>'+
+                    '<a href="#/tasks" class="list-group-item list-group-item-info">Delayed Jobs<span class="badge">{{delayedJobs}}</span></a>'+
+                    '<a href="#/tasks" class="list-group-item list-group-item-danger">Failed Jobs<span class="badge">{{failedJobs}}</span></a>'+
+                  '</ul>'+
+                  '<ul class="list-group">'+
+                    '<li class="list-group-item" ng-repeat="(workerName, taskCount) in processedJobs">{{workerName}}<span class="badge">{{taskCount}}</span></li>'+
+                  '</ul>'+
+                '</div>',
       link: function (scope) {
         $.get('/api/getAllRunningJobs', function (data) {
           scope.runningJobs = data.runningJobs.length;
@@ -14,6 +23,9 @@ define(['app'], function(app){
         });
         $.get('/api/getAllFailedJobs', function (data) {
           scope.failedJobs = data.failedJobs.length;
+        });
+        $.get('/api/getTasksStatistics', function (data) {
+          scope.processedJobs = data.processedJobs;
         });
       }
     };
