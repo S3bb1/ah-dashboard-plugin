f11d59f948eb56a90e4ecdb0090281fa25b6c6fa
David Schlechtweg <David.Schlechtweg@b-s-s.de>
2014-10-16 09:06:26 +0200

    add display of readme and changelog informations in plugin overview as modal dialog by selecting the plugin name or plugin version.

--- public/dashboard/js/controllers/ahDashboardPluginsController.js
+++ public/dashboard/js/controllers/ahDashboardPluginsController.js
@@ -1,9 +1,19 @@
 define(['app'], function (app) {
-  app.controller('ahDashboardPlugins', function ($scope) {
+  app.controller('ahDashboardPlugins', function ($scope, ngDialog) {
     $.get('/api/getPlugins', function (data) {
       $scope.plugins = data.plugins;
       $scope.pluginsLoadingDone = true;
       $scope.$apply();
+
+      $scope.open = function (html) {
+        ngDialog.open({
+          template: html,
+          plain: true,
+          className: 'ngdialog-theme-default',
+          scope: $scope
+        });
+      };
+
     });
   });
 });
