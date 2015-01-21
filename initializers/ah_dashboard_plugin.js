f08cef847113fe3300bdd5b1f4ac4f802a36b6ec
Johannes Hense <johannes.hense@b-s-s.de>
2015-01-21 10:27:01 +0100

    added file exists check on log file to prevent apphero boot problems if log files are create somewhere else

--- initializers/ah_dashboard_plugin.js
+++ initializers/ah_dashboard_plugin.js
@@ -1,24 +1,28 @@
 var ah_dashboard_plugin = function(api, next){
   var path = require('path');
+  var fs = require('fs');
   var TS = require('redis-timeseries');
   var Tail = require('always-tail');
   var timeSeries = new TS(api.redis.client);
   api.ahDashboard = {};
   api.ahDashboard.timesSeries = timeSeries;
   api.ahDashboard.prevStats = {};
-  api.chatRoom.add("logMessages");
-  var spawn = require('child_process').spawn;
 
+  var logFile = api.config.general.paths.log[0] + path.sep + api.pids.title + '.log';
+  if(fs.existsSync(logFile)) {
+    api.chatRoom.add("logMessages");
+    tail = new Tail(logFile);
 
-  tail = new Tail(api.config.general.paths.log[0] + path.sep + api.pids.title + '.log');
+    tail.on("line", function (data) {
+      api.chatRoom.broadcast({room: "logMessages"}, "logMessages", data.toString());
+    });
 
-  tail.on("line", function(data) {
-    api.chatRoom.broadcast({room: "logMessages"}, "logMessages", data.toString());
-  });
-
-  tail.on("error", function(error) {
-    console.log('ERROR: ', error);
-  });
+    tail.on("error", function (error) {
+      console.log('ERROR reading log file: ' + error);
+    });
+  } else {
+    console.log('Cant locate log files, log file view in dashboard is disabled.')
+  }
   next();
 }
 
