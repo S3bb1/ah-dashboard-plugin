b20899caed0a2c940c3afdd6839b2a73b67f75ec
sonhuy.pham <sonhuy.pham@samsung.com>
2015-08-25 14:21:48 -0700

    Updated communcationService to jive with the latest version of ahClient - but also updated logins to post credentials in the body instead of the querystring

--- public/dashboard/app/services/communicationService.js
+++ public/dashboard/app/services/communicationService.js
@@ -3,7 +3,22 @@ define(['app'], function (app) {
     var ahClient = new actionheroClient();
     var actionEmit = function(action, params, callback){
       if(ahDashboardSession || (action === "login" || action === "logout" || action === "currentUser")){
-        ahClient.action(action, params, callback);
+        var ahCallback = function(response) {
+          // Based on the latest version of actionheroClient.js, error is stuffed into the response.
+          if(response && response.error) {
+            if(typeof callback === 'function') callback(response.error, response);
+            else if(typeof params === 'function') params(response.error, response);
+          } else {
+            if(typeof callback === 'function') callback(null, response);
+            else if(typeof params === 'function') params(response.error, response);
+          }
+        };
+        if(action === "login") {
+          params.action = "login";
+          ahClient.actionWeb(params, ahCallback);
+          return;
+        }
+        ahClient.action(action, params, ahCallback);
       }
       return;
     };
